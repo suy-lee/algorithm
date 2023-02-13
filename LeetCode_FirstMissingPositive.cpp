40099f6a5ee69ce4fcad716a65b5d9e339e72745
suy-lee <suy.suy.lee@gmail.com>
2023-02-14 08:26:40 +0900

    Add solution for problem 'First Missing Positive'
    Signed-off-by: suy-lee <suy.suy.lee@gmail.com>

--- LeetCode_FirstMissingPositive.cpp
+++ LeetCode_FirstMissingPositive.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int hash = nums.size() + 1;
+        int n = nums.size();
+        int hash = n + 1;
+        int idx = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] <= 0 || hash <= nums[i]) nums[i] = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] >= hash) nums[i] = 0;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            int idx = nums[i] % hash;
-            if (idx > 0) nums[idx - 1] += hash;
+        for (int i = 0; i < n; i++) {
+            idx = (nums[i] % hash) - 1;
+            if (idx >= 0) nums[idx] += hash;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] / hash == 0) return i+1;
         }
 

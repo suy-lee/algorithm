58ea5b7fd91eb73120879d0d22370203d92826c4
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-08-28 00:10:49 +0900

    Time: 14 ms (88.34%), Space: 13.5 MB (48.25%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     void dfs(vector<int> adjList[], vector<int> &visited, int src) {
         visited[src] = 1;
-        
+
         for (int i = 0; i < adjList[src].size(); i++) {
-            if (visited[adjList[src][i]] == 0) {
+            if (visited[adjList[src][i]] == 0) // if src to i edge is not visited yet
                 dfs(adjList, visited, adjList[src][i]);
-            }
         }
     }
 
     int countComponents(int n, vector<vector<int>>& edges) {
         if (n == 0) return 0;
+
         int comp = 0;
-        vector<int> visited(n, 0);
+        vector<int> visited(n, 0) ;
         vector<int> adjList[n];
 
         for (int i = 0; i < edges.size(); i++) {
@@ -21,7 +21,7 @@ public:
             adjList[edges[i][1]].push_back(edges[i][0]);
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {   // n == size of vertices
             if (visited[i] == 0) {
                 comp++;
                 dfs(adjList, visited, i);

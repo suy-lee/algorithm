3dd6cb32fb03cc6731b742b0c2ff2705f728db95
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-08-28 01:14:06 +0900

    Time: 24 ms (54.44%), Space: 14.5 MB (20.8%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>> &adj, vector<bool> &visit, vector<bool> &inStack) {
+        // if the node is already in the stack, we have a cycle.
+        if (inStack[node]) {
+            return true;
+        }
+
+        if (visit[node]) {
+            return false;
+        }
+
+        // mark the current node as visited and part of current recursion stack.
+        visit[node] = true;
+        inStack[node] = true;
+
+        for (auto neighbor : adj[node]) {
+            if (dfs(neighbor, adj, visit, inStack)) {
+                return true;
+            }
+        }
+
+        // remove the node from the stack.
+        inStack[node] = false;
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // vector prerequisites [a, b]
+        // a = course that you want to take
+        // b = prerequisite for a
+        // cycle may be exist
+        
+        vector<vector<int>> adj(numCourses);
+
+        for (auto prerequisite : prerequisites) {
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+        }
+
+        vector<bool> visit(numCourses);
+        vector<bool> inStack(numCourses);
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, adj, visit, inStack)) {
+                return false;   // when cycle detected
+            }
+        }
+
+        return true;
+    }
+};

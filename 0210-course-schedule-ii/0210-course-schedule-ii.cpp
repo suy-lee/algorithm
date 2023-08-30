c6c2deb01f98897ab9dd26c455c052002934021a
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-08-31 01:05:40 +0900

    Time: 17 ms (77.29%), Space: 13 MB (95.2%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,36 +2,42 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         // BFS approach
-        vector<int>adj[numCourses];
-        queue<int>q;
-        vector<int>indegree(numCourses,0);
-        //store 
-         for(int i =0; i< prerequisites.size(); i++ ){
+        vector<int> adj[numCourses];
+        queue<int> q;
+        vector<int> indegree(numCourses, 0);
+
+        // Store adj node
+         for(int i = 0; i < prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        //indegree u -> v
-        for(int i =0; i < prerequisites.size(); i++){
+        // Calc indegree u -> v
+        for(int i = 0; i < prerequisites.size(); i++){
             indegree[prerequisites[i][0]]++;
-            
         }
-        //push into q,indegree having 0
-        for(int i =0; i< indegree.size(); i++){
-            if(indegree[i] == 0)q.push(i);
+
+        // Push node which has indegree of 0
+        for(int i = 0; i< indegree.size(); i++){
+            if (indegree[i] == 0) q.push(i);
         }
+
         vector<int>result;
-        //Simple BFS and keep storing indegree values equal to 0 into result vector
-        while(!q.empty()){
+
+        // Simple BFS and keep storing indegree values equal to 0 into result vector
+        while (!q.empty()){
            int v = q.front();
-            q.pop();
-            for(auto it : adj[v]){
+           q.pop();
+   
+           for(auto it : adj[v]){ // for all adjacent nodes
                 indegree[it]--;
-                 if(indegree[it] == 0)q.push(it);
+                if (indegree[it] == 0) q.push(it);
             }
             result.emplace_back(v);
         }
-        //check case 
+
+        // Check case
         if (result.size() != numCourses)
-        result.clear();
+            result.clear();
+
         return result;
     }
 };

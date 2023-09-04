80068514f15e162642e1b8f572243181519c8470
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-09-05 00:17:57 +0900

    Time: 467 ms (64.5%), Space: 8.1 MB (44.27%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-        if (!word.size())
-            return true;
+    bool dfs(vector<vector<char>>& board, int i, int j, string &word) {
+        // If word to find size is zero
+        if (!word.size()) return true;
 
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[0])  
-            return false;
+        // Boundary check
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() ||
+            word[0] != board[i][j]) return false;
 
         char c = board[i][j];
-        board[i][j] = '*';          // mark as visited
-        string s = word.substr(1);  // delete first character from string
+        board[i][j] = '*';
+        string s = word.substr(1);
 
-        bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
+        bool result = dfs(board, i-1, j, s) || 
+                        dfs(board, i+1, j, s) ||
+                        dfs(board, i, j-1, s) || 
+                        dfs(board, i, j+1, s);
 
-        board[i][j] = c;            // restore
-        return ret;
+        board[i][j] = c;
+        return result;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-        for (unsigned int i = 0; i < board.size(); i++) 
-            for (unsigned int j = 0; j < board[0].size(); j++) 
-                if (dfs(board, i, j, word))
-                    return true;
-        return false; 
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (dfs(board, i, j, word)) return true;
+            }
+        }
+        return false;
     }
 };

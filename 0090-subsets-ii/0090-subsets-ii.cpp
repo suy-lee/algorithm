fdd616e1261c3acf9bbad698efa60bcf5f64a9ed
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-09-07 12:51:39 +0900

    Time: 4 ms (53.26%), Space: 7.5 MB (76.90%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> subsets;
+        vector<int> currentSubset;
+        sort(nums.begin(), nums.end());
+        subsetsWithDupHelper(subsets, currentSubset, nums, 0);
+        return subsets;
+    }
+    
+private:
+    void subsetsWithDupHelper(vector<vector<int>> &subsets, vector<int> &currentSubset, vector<int> &nums, int index) {
+        // Add the subset formed so far to the subsets list.
+        subsets.push_back(currentSubset);
+
+        for (int i = index; i < nums.size(); i++) {
+            // If the current element is a duplicate, ignore.
+            if (i != index && nums[i] == nums[i-1]) {   // since array is sorted, duplicate number should be place adjacent
+                continue;
+            }
+            
+            currentSubset.push_back(nums[i]);
+            subsetsWithDupHelper(subsets, currentSubset, nums, i+1);
+            currentSubset.pop_back();
+        }
+    }
+};

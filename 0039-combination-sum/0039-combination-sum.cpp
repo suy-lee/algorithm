24cddfe0b66a3eabfdadbcb659b1fee0ec863782
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-09-09 23:03:53 +0900

    Time: 4 ms (77.35%), Space: 10.7 MB (68.67%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(candidates, target, 0, curr, ans);
+        return ans;
+    }
+    
+    void helper(vector<int>& candidates, int target, int start, vector<int>& curr, vector<vector<int>>& ans){
+        if(target == 0){
+            ans.push_back(curr);
+            return;
+        }
+        if(target < 0){
+            return;
+        }
+        for(int i=start; i<candidates.size(); i++){
+            curr.push_back(candidates[i]);
+            helper(candidates, target-candidates[i], i, curr, ans);
+            curr.pop_back();
+        }
+    }
+};

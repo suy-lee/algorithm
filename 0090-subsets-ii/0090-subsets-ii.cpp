a690138a59559001e05b5ce202d31738fe8289d6
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-09-13 23:20:32 +0900

    Time: 0 ms (100.00%), Space: 8.2 MB (37.24%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> subsets;
-        vector<int> current;
-        sort(nums.begin(), nums.end());
-        subsetsWithDupHelper(subsets, current, nums, 0);
-        return subsets;
-    }
-
-private:
-    void subsetsWithDupHelper(vector<vector<int>> &subsets, vector<int> &current, vector<int> &nums, int index) {
-        subsets.push_back(current);
+    vector<vector<int>> ans;
+    vector<int> curr;
+    
+    void backtrack(int index, vector<int> &nums, vector<int> &curr) {
+        ans.push_back(curr);
         
         for (int i = index; i < nums.size(); i++) {
-            if (i != index && nums[i] == nums[i-1]) continue; // get rid of duplicate
-            
-            current.push_back(nums[i]);
-            subsetsWithDupHelper(subsets, current, nums, i+1);
-            current.pop_back();
+            if (i != index && nums[i] == nums[i-1]) continue;
+
+            curr.push_back(nums[i]);
+            backtrack(i+1, nums, curr);
+            curr.pop_back();
         }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        backtrack(0, nums, curr);
+        
+        return ans;
     }
 };

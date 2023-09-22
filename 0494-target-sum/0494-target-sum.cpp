bc991b2e1a930a68d467bdd2464ab05b48ed73c9
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-09-23 00:34:18 +0900

    Time: 8 ms (86.72%), Space: 9.6 MB (61.20%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int dp[21][2001];
     
-    int dfs(int index, vector<int> &nums, int &S, int sum) {
-        // 1 is this is the path
-        // 0 means not
-        if (index == nums.size())
-            return sum == S? 1 : 0;
+    int dfs(vector<int> &nums, int index, int S, int sum) {
+        if (index == nums.size()) {
+            if (sum == S) return 1;
+            else return 0;
+        }
         
-        // if precalculated value is stored, return it
         if (dp[index][sum+1000] != -1) return dp[index][sum+1000];
         
         int count = 0;
         
-        count += dfs(index+1, nums, S, sum+nums[index]);
-        count += dfs(index+1, nums, S, sum-nums[index]);
+        count += dfs(nums, index+1, S, sum+nums[index]);
+        count += dfs(nums, index+1, S, sum-nums[index]);
         
-        return dp[index][sum+1000] = count;
+        return dp[index][sum+1000] = count;     
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         memset(dp, -1, sizeof(dp));
         
-        return dfs(0, nums, target, 0);
+        return dfs(nums, 0, target, 0);
     }
 };

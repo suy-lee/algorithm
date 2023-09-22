09f0dc91b36d44db3891af79e82a873dd291af84
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-09-22 23:02:15 +0900

    Time: 100 ms (68.08%), Space: 60.3 MB (6.56%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,13 +3,13 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int l = 0, r = arr.size() - 1, mid;
         
-        while (l < r) {
-            mid = (l + r) / 2;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
             
-            if (arr[mid] < arr[mid + 1])
+            if (arr[mid] < arr[mid+1])
                 l = mid + 1;
             else
-                r = mid;
+                r = mid - 1;
         }
         
         return l;

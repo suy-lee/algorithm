2b4c91869cf50823e6c71936e3cb45901b710ca4
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-10-17 23:53:42 +0900

    Time: 3 ms (36.62%), Space: 8 MB (46.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
-
-        int dp[n+1];
+        int dp[nums.size() + 1];
+        if (nums.size() == 1) return nums[0];
         memset(dp, 0, sizeof(dp));
         
         dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++)
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
-
-        return dp[n-1];
+        dp[1] = max(nums[1], nums[0]);
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        
+        return dp[nums.size()-1];
     }
 };

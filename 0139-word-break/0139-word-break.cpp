c6e6c2325d720362b0a34e9cf2b8e2f5993ed142
Suyeon Lee <44089681+suy-lee@users.noreply.github.com>
2023-10-18 00:59:11 +0900

    Time: 5 ms (72.96%), Space: 8.4 MB (75.29%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -4,24 +4,18 @@ public:
     vector<string> wordDict;
     string s;
     
-    bool wordBreak(string s, vector<string>& wordDict) {
-        dp = vector<int>(s.size(), -1);
-        this->wordDict = wordDict;
-        this->s = s;
-        return find(s.size()-1);
-    }
-    
     bool find(int i) {
         if (i < 0) return true; // boundary check
         
-        if (dp[i] != -1) // has memoization value
-            return dp[i] == 1? true: false;
+        if (dp[i] != -1)    // has memoization value
+            return dp[i] == 1? true : false;
         
         for (string word: wordDict) {
-            // get length of current word from wordDict
+            // get the length of current word
             int currSize = word.size();
+            
             // boundary check
-            if (i - currSize + 1 < 0) 
+            if (i - currSize + 1 < 0)
                 continue; // do not check this word
             
             // if word matches && recursively call i - currSize
@@ -33,5 +27,12 @@ public:
         
         dp[i] = 0;
         return false;
+    }
+ 
+    bool wordBreak(string s, vector<string>& wordDict) {
+        dp = vector<int>(s.size(), -1);
+        this->wordDict = wordDict;
+        this->s = s;
+        return find(s.size() - 1);
     }
 };
